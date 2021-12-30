c1f1a974d38d57ea76f7e59bcc149300f43c7000
ian <im-ian@naver.com>
2021-12-30 21:41:43 +0900

    BE, FE 선택 설치 지원, zsh theme&plugin 설치 지원

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,18 +1,16 @@
-# !/bin/bash
-
 # install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+env RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # copy my zsh settings
 cp ./zsh/zshrc ~/.zshrc
 
-# install zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
-
 # install zsh theme
-git clone https://github.com/romkatv/powerlevel10k.git /Users/$USER/.oh-my-zsh/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git /Users/"$USER"/.oh-my-zsh/themes/powerlevel10k
+
+# install zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 # apply config
 source ~/.zshrc
